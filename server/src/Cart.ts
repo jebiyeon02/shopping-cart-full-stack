45f000596007453765f90964dae13f6c7d870b3b
김예안 <kjan0929@naver.com>
2026-05-27 21:40:25 +0900

    feat: 장바구니 상품 수량 변경 필수 필드 누락 예외 처리 및 테스트 작성
    Co-authored-by: JeongYeonJun <jebiyeon02@users.noreply.github.com>

--- server/src/Cart.ts
+++ server/src/Cart.ts
@@ -11,19 +11,22 @@ class Cart {
     this.cartItems.set(2, 5);
   }
 
-  //   private validateOrderCount(quantity: number) {
-  //     if (!quantity && quantity !== 0) {
-  //       throw new Error('재고 필드가 누락되었습니다.');
-  //     }
-  //     if (typeof quantity === 'string' || quantity < 1 || quantity > 99) {
-  //       throw new Error('상품 재고는 1이상 99이하의 정수이어야 합니다.');
-  //     }
-  //   }
+  private validateOrderCount(id: number, orderCount: number) {
+    if (!this.cartItems.has(id)) {
+      throw new Error('수량을 변경하려는 상품이 존재하지 않습니다.');
+    }
 
-  setOrderCount(id: number, orderCount: number) {
+    if (!orderCount && orderCount !== 0) {
+      throw new Error('주문 수량 필드가 누락되었습니다.');
+    }
+    
     if (typeof orderCount === 'string' || orderCount < 1) {
       throw new Error('변경할 수량은 0보다 큰 숫자여야 합니다.');
     }
+  }
+
+  setOrderCount(id: number, orderCount: number) {
+    this.validateOrderCount(id, orderCount);
     this.cartItems.set(id, orderCount);
   }
 
--- server/src/__tests__/carts.test.ts
+++ server/src/__tests__/carts.test.ts
@@ -81,6 +81,40 @@ describe('장바구니 상품 수량 변경 예외 테스트', () => {
       cart.setOrderCount(productId, '2');
     }).toThrow('변경할 수량은 0보다 큰 숫자여야 합니다.');
   });
+
+  test('장바구니에 존재하지 않는 상품의 수량을 변경하면 에러를 발생시킨다.', () => {
+    // given
+    const cart = new Cart();
+    const wrongProductId = 999;
+
+    // when & then
+    expect(() => {
+      // @ts-ignore 예외 처리를 위한 타입 무시
+      cart.setOrderCount(wrongProductId, 2);
+    }).toThrow('수량을 변경하려는 상품이 존재하지 않습니다.');
+  });
+
+  test('필수 필드 누락 시 에러를 응답한다.', () => {
+    // given
+    const cart = new Cart();
+    const productId = 1;
+
+    // when & then
+    expect(() => {
+      // @ts-ignore 예외 처리를 위한 타입 무시
+      cart.setOrderCount(productId, '');
+    }).toThrow('주문 수량 필드가 누락되었습니다.');
+
+    expect(() => {
+      // @ts-ignore 예외 처리를 위한 타입 무시
+      cart.setOrderCount(productId, null);
+    }).toThrow('주문 수량 필드가 누락되었습니다.');
+
+    expect(() => {
+      // @ts-ignore 예외 처리를 위한 타입 무시
+      cart.setOrderCount(productId, undefined);
+    }).toThrow('주문 수량 필드가 누락되었습니다.');
+  });
 });
 
 // 1. 남아있는 재고보다 더 많은 값으로 변경하려고 함
