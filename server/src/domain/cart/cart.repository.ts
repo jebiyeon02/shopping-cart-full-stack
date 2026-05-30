bc199dbcbd2f19497811118e629f929f07bcbdac
jebiyeon02 <jhje5595@naver.com>
2026-05-31 03:57:02 +0900

    feat: 장바구니 도메인 계층 추가

---
+++ server/src/domain/cart/cart.repository.ts
@@ -0,0 +1,51 @@
+import CartItem, { CartItemType } from "../../model/CartItem.js";
+
+export interface CartRepository {
+  add: (productId: number, itemCount: number) => void;
+  delete: (productId: number) => void;
+  get: () => CartItemType[];
+  reset: () => void;
+  updateItemCount: (productId: number, itemCount: number) => boolean;
+  findByProductId: (productId: number) => CartItem | undefined;
+}
+
+export class InMemoryCartRepository implements CartRepository {
+  private cart: Array<CartItem>;
+
+  constructor() {
+    this.cart = [];
+  }
+
+  add(productId: number, itemCount: number) {
+    const newCartItem = new CartItem(productId, itemCount);
+    this.cart.push(newCartItem);
+  }
+
+  findByProductId(productId: number) {
+    return this.cart.find((cartItem) => cartItem.isSameProductId(productId));
+  }
+
+  delete(productId: number) {
+    this.cart = this.cart.filter(
+      (cartItem) => !cartItem.isSameProductId(productId),
+    );
+  }
+
+  updateItemCount(productId: number, itemCount: number) {
+    this.cart.forEach((cartItem) => {
+      if (cartItem.isSameProductId(productId)) {
+        cartItem.updateItemCount(itemCount);
+      }
+    });
+
+    return true;
+  }
+
+  get() {
+    return this.cart.map((cartItem) => cartItem.toJson());
+  }
+
+  reset() {
+    this.cart = [];
+  }
+}
---
+++ server/src/domain/cart/cart.service.ts
@@ -0,0 +1,76 @@
+import AppError from "../../errors/AppError.js";
+import { ProductRepository } from "../product/product.repository.js";
+import { CartRepository } from "./cart.repository.js";
+
+class CartService {
+  constructor(
+    private cartRepository: CartRepository,
+    private productRepository: ProductRepository,
+  ) {}
+
+  getCartItems() {
+    const cartItems = this.cartRepository.get();
+
+    return cartItems.map((cartItem) => {
+      const product = this.productRepository.findById(cartItem.productId);
+      if (!product) {
+        throw new Error("불일치 에러");
+      }
+
+      return {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        imgUrl: product.imgUrl,
+        itemCount: cartItem.itemCount,
+      };
+    });
+  }
+
+  addCartItem(productId: number, itemCount: number) {
+    this.cartRepository.add(productId, itemCount);
+
+    return productId;
+  }
+
+  deleteCartItem(id: number) {
+    const targetCartItem = this.cartRepository.findByProductId(id);
+    if (!targetCartItem) {
+      throw new AppError("PRODUCT_NOT_EXIST_IN_CART");
+    }
+    this.cartRepository.delete(id);
+  }
+
+  updateItemCount(productId: number, itemCount: number) {
+    const cartItem = this.cartRepository.findByProductId(productId);
+
+    if (!cartItem) {
+      throw new AppError("PRODUCT_NOT_EXIST_IN_CART");
+    }
+
+    const product = this.productRepository.findById(
+      cartItem.toJson().productId,
+    );
+
+    if (!product) {
+      throw new AppError("PRODUCT_NOT_EXIST");
+    }
+
+    if (product!.quantity < itemCount) {
+      throw new AppError("PRODUCT_ORDER_COUNT_EXCEEDED");
+    }
+
+    cartItem.updateItemCount(itemCount);
+
+    return {
+      productId,
+      itemCount,
+    };
+  }
+
+  reset() {
+    this.cartRepository.reset();
+  }
+}
+
+export default CartService;
