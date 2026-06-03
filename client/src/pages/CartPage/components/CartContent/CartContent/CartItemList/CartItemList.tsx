f65bd753ed8fb7fd81772b8ad4b1f7b241b03637
jebiyeon02 <jhje5595@naver.com>
2026-06-04 00:19:34 +0900

    feat: Cart페이지에서 사용되는 컴포넌트 구현 및 데이터 흐름 정의

---
+++ client/src/pages/CartPage/components/CartContent/CartContent/CartItemList/CartItemList.tsx
@@ -0,0 +1,5 @@
+const CartItemList = () => {
+  return <div>카트 아이템 리스트</div>;
+};
+
+export default CartItemList;
---
+++ client/src/pages/CartPage/components/CartContent/CartContent/CartPaymentSummary.tsx
@@ -0,0 +1,11 @@
+const CartPaymentSummary = ({
+  orderPrice,
+  deliveryFee,
+}: {
+  orderPrice: number;
+  deliveryFee: number;
+}) => {
+  return <div>장바구니 결제 정보 요약</div>;
+};
+
+export default CartPaymentSummary;
