d9106146c62a88c7eaa43c45302c52bb47b3f98b
jebiyeon02 <jhje5595@naver.com>
2026-06-03 20:33:33 +0900

    feat(cart): 장바구니 상품 추가/삭제/조회 api요청 메서드 구현

---
+++ client/src/domain/cart/cart.api.ts
@@ -0,0 +1,87 @@
+import ApiError from "../../error/ApiError";
+
+const BASE_URL = "http://localhost:3000";
+
+type ApiResponse<T> = {
+  code: string;
+  message: string;
+  result: T;
+};
+
+type ApiErrorResponse = {
+  code: string;
+  message: string;
+};
+
+export type CartItemResponse = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string | null; // TODO: 서버에서 처리를 잘 못해주는 것 같음
+  itemCount: number;
+};
+
+export const getCartItems = async (
+  cartId: number,
+): Promise<CartItemResponse[]> => {
+  const response = await fetch(`${BASE_URL}/carts/${cartId}/items`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const errorData: ApiErrorResponse = await response.json();
+    throw new ApiError(errorData.code, errorData.message);
+  }
+
+  const data: ApiResponse<{ cartItems: CartItemResponse[] }> =
+    await response.json();
+  const cartItems = data.result.cartItems;
+
+  return cartItems;
+};
+
+export const addCartItem = async (
+  cartId: number,
+  productId: number,
+  itemCount: number,
+): Promise<number> => {
+  const response = await fetch(`${BASE_URL}/carts/${cartId}/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      productId,
+      itemCount,
+    }),
+  });
+
+  if (!response.ok) {
+    const errorData: ApiErrorResponse = await response.json();
+    throw new ApiError(errorData.code, errorData.message);
+  }
+
+  const data: ApiResponse<{ productId: number }> = await response.json();
+  const addedProductId = data.result.productId;
+
+  return addedProductId;
+};
+
+export const deleteCartItem = async (
+  cartId: number,
+  productId: number,
+): Promise<void> => {
+  const response = await fetch(
+    `${BASE_URL}/carts/${cartId}/items/${productId}`,
+    {
+      method: "DELETE",
+    },
+  );
+
+  if (!response.ok) {
+    const errorData: ApiErrorResponse = await response.json();
+    throw new ApiError(errorData.code, errorData.message);
+  }
+};
