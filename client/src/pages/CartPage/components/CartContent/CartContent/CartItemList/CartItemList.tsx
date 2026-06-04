6c69a4f936530465c2abc38bf06663b3be2ae913
jebiyeon02 <jhje5595@naver.com>
2026-06-04 14:28:29 +0900

    feat(cart): 전체 선택 및 해제 기능 추가

--- client/src/pages/CartPage/components/CartContent/CartContent/CartItemList/CartItemList.tsx
+++ client/src/pages/CartPage/components/CartContent/CartContent/CartItemList/CartItemList.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import type { CartItemResponse } from "../../../../../../domain/cart/cart.api";
 import CartItem from "./CartItem";
 
 const CartItemList = ({
   cartItems,
   onDeleteCartItem,
+  onAllProductSelect,
 }: {
   cartItems: CartItemResponse[];
   onDeleteCartItem: (productId: number) => void;
+  onAllProductSelect: (action: "fill" | "empty") => void;
 }) => {
+  // TODO: 로컬 스토리지로 변경 필요
+  const [isSelectAllProduct, setIsSelectAllProduct] = useState(false);
   return (
     <div>
+      <input
+        type="checkbox"
+        onChange={() => {
+          if (isSelectAllProduct) {
+            onAllProductSelect("empty");
+          } else {
+            onAllProductSelect("fill");
+          }
+          setIsSelectAllProduct((prev) => !prev);
+        }}
+      />{" "}
+      전체 선택
       {cartItems.map((cartItem) => (
         <CartItem
           key={cartItem.id}
