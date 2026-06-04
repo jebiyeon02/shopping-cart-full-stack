153ddba0f48d9923fc450b098b871ad5e233c4da
jebiyeon02 <jhje5595@naver.com>
2026-06-04 20:18:24 +0900

    feat(cart): 장바구니 스타일 적용

--- client/src/pages/CartPage/components/CartContent/CartContent/CartPaymentSummary.tsx
+++ client/src/pages/CartPage/components/CartContent/CartContent/CartPaymentSummary.tsx
@@ -1,3 +1,6 @@
+import styled from "@emotion/styled";
+import { formatPrice } from "../../../../../shared/utils";
+
 const CartPaymentSummary = ({
   orderPrice,
   deliveryFee,
@@ -5,7 +8,58 @@ const CartPaymentSummary = ({
   orderPrice: number;
   deliveryFee: number;
 }) => {
-  return <div>{`주문 금액: ${orderPrice}, 배송비: ${deliveryFee}`}</div>;
+  const totalPrice = orderPrice + deliveryFee;
+
+  return (
+    <CartPaymentSummaryLayout>
+      <SummaryTable>
+        <tbody>
+          <SummaryTr>
+            <SummaryTh scope="row">주문 금액</SummaryTh>
+            <SummaryTd>{formatPrice(orderPrice)}원</SummaryTd>
+          </SummaryTr>
+          <SummaryTr $hasDivider>
+            <SummaryTh scope="row">배송비</SummaryTh>
+            <SummaryTd>{formatPrice(deliveryFee)}원</SummaryTd>
+          </SummaryTr>
+          <SummaryTr>
+            <SummaryTh scope="row">총 결제 금액</SummaryTh>
+            <SummaryTd>{formatPrice(totalPrice)}원</SummaryTd>
+          </SummaryTr>
+        </tbody>
+      </SummaryTable>
+    </CartPaymentSummaryLayout>
+  );
 };
 
 export default CartPaymentSummary;
+
+const CartPaymentSummaryLayout = styled.div`
+  margin-top: 32px;
+`;
+
+const SummaryTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  border-top: 1px solid #0000001a;
+`;
+
+const SummaryTr = styled.tr<{ $hasDivider?: boolean }>`
+  border-bottom: ${({ $hasDivider }) =>
+    $hasDivider ? "1px solid #0000001a" : "none"};
+`;
+
+const SummaryTh = styled.th`
+  color: black;
+  font-weight: 700;
+  font-size: 16px;
+  text-align: left;
+`;
+
+const SummaryTd = styled.td`
+  padding: 12px;
+  color: black;
+  font-weight: 700;
+  font-size: 24px;
+  text-align: right;
+`;
