515e118a3ec0d60e3f24606a344e606b63b8c27d
jebiyeon02 <jhje5595@naver.com>
2026-06-04 23:30:24 +0900

    feat: react-router-dom을 이용한 페이지 라우팅 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CartPage from "./pages/CartPage/CartPage";
+import OrderConfirmPage from "./pages/OrderConfirmPage/OrderConfirmPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="cart" element={<CartPage cartId={1}></CartPage>} />
+      <Route path="cart/order-confirm" element={<OrderConfirmPage />} />
+    </Routes>
+  );
 }
 
 export default App;
