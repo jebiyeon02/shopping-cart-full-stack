9823833875c8c3173e689328158e04e9bcb07c6e
jebiyeon02 <jhje5595@naver.com>
2026-06-04 21:05:13 +0900

    feat: disabled 버튼 스타일 구현

--- client/src/shared/components/BaseButton.tsx
+++ client/src/shared/components/BaseButton.tsx
@@ -3,8 +3,16 @@ import type { ComponentPropsWithRef } from "react";
 
 type BaseButtonProps = ComponentPropsWithRef<"button">;
 
-const BaseButton = ({ children, type = "button" }: BaseButtonProps) => {
-  return <Button type={type}>{children}</Button>;
+const BaseButton = ({
+  children,
+  type = "button",
+  disabled = false,
+}: BaseButtonProps) => {
+  return (
+    <Button type={type} disabled={disabled}>
+      {children}
+    </Button>
+  );
 };
 
 export default BaseButton;
@@ -12,9 +20,16 @@ export default BaseButton;
 const Button = styled.button`
   width: 100%;
   height: 64px;
+  border: none;
   background-color: black;
   font-weight: 700;
   font-size: 16px;
   color: white;
   cursor: pointer;
+
+  &:disabled {
+    background-color: #bebebe;
+    opacity: 1;
+    cursor: not-allowed;
+  }
 `;
