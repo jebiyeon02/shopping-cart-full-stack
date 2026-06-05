b4c38b36c17edad67b14357effc3f0be1b86e3c0
jebiyeon02 <jhje5595@naver.com>
2026-06-05 14:21:24 +0900

    feat: 장바구니 상품 삭제/수량 업데이트 중복 api 요청 방지

--- client/src/pages/CartPage/useCartItem.ts
+++ client/src/pages/CartPage/useCartItem.ts
@@ -6,20 +6,28 @@ import {
   type CartItemResponse,
 } from "../../domain/cart/cart.api";
 import { errorHandler } from "../../error/errorHandler";
+import useAsyncState from "../../useAsyncState";
 // import ApiError from "../../error/ApiError";
 
 // 이 훅은 서버상태인 cartItem을 관리하는 훅임
 // 실제로는 그럴일이 없겠지만, 비로그인 상태에서 여러 카트에 담을 수 있다고 가정
 const useCartItem = (cartId: number) => {
   const [cartItems, setCartItems] = useState<CartItemResponse[] | null>(null);
+  const getCartItemsAsyncState = useAsyncState();
+  const deleteCartItemAsyncState = useAsyncState();
+  const updateCartItemCountAsyncState = useAsyncState();
 
   const loadCartItems = async () => {
     try {
+      getCartItemsAsyncState.setLoading();
+
       const fetchedCartItems = await getCartItems(cartId);
 
       setCartItems(fetchedCartItems);
+      getCartItemsAsyncState.setSuccess();
     } catch (error) {
       errorHandler(error);
+      getCartItemsAsyncState.setFail();
     }
   };
 
@@ -32,10 +40,14 @@ const useCartItem = (cartId: number) => {
 
   const requestDeleteCartItem = async (productId: number) => {
     try {
+      deleteCartItemAsyncState.setLoading();
+
       await deleteCartItem(cartId, productId);
       await loadCartItems();
+      deleteCartItemAsyncState.setSuccess();
     } catch (error) {
       errorHandler(error);
+      deleteCartItemAsyncState.setFail();
     }
   };
 
@@ -44,14 +56,25 @@ const useCartItem = (cartId: number) => {
     itemCount: number,
   ) => {
     try {
+      updateCartItemCountAsyncState.setLoading();
+
       await updateCartItemCount(cartId, productId, itemCount);
       await loadCartItems();
+      updateCartItemCountAsyncState.setSuccess();
     } catch (error) {
       errorHandler(error);
+      updateCartItemCountAsyncState.setFail();
     }
   };
 
-  return { cartItems, requestDeleteCartItem, requestUpdateCartItemCount };
+  return {
+    cartItems,
+    requestDeleteCartItem,
+    requestUpdateCartItemCount,
+    getCartItemsAsyncState: getCartItemsAsyncState.asyncState,
+    deleteCartItemAsyncState: deleteCartItemAsyncState.asyncState,
+    updateCartItemCountAsyncState: updateCartItemCountAsyncState.asyncState,
+  };
 };
 
 export default useCartItem;
