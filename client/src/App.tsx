c8639bb2456fee923b9d8827dd22797a7b594705
jebiyeon02 <jhje5595@naver.com>
2026-06-19 22:46:07 +0900

    feat: 주문확인 페이지 라우팅 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import CartPage from "./pages/CartPage/CartPage";
 import OrderConfirmPage from "./pages/OrderConfirmPage/OrderConfirmPage";
 import { CartProvider } from "./pages/CartPage/CartContext";
 import { CartSelectionProvider } from "./pages/CartPage/CartSelectionContext";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           </CartProvider>
         }
       />
+      <Route path="checkout/:id" element={<CheckoutPage />} />
       <Route path="cart/order-confirm" element={<OrderConfirmPage />} />
     </Routes>
   );
