2f0f18ef06b0b9bd08a02b1253c09c1c13618073
jebiyeon02 <jhje5595@naver.com>
2026-06-20 19:47:43 +0900

    feat: 도서산간 checkbox 추가 및 도서산간 api 연결

--- client/src/pages/CheckoutPage/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage/CheckoutPage.tsx
@@ -2,23 +2,50 @@ import { useState } from "react";
 import CheckoutItemList from "./CheckoutItemList/CheckoutItemList";
 import CheckoutCouponApplyButton from "./CheckoutCouponApplyButton";
 import CheckoutPaymentSummary from "./CheckoutPaymentSummary";
-import type { CheckoutContent } from "../../domain/checkout/checkout.api";
+
 import CheckoutCouponModal from "./CheckoutCouponModal/CheckoutCouponModal";
 
+import { useParams } from "react-router-dom";
+import { useCheckoutContent } from "./CheckoutItemList/useCheckoutContent";
+
 const CheckoutPage = () => {
-  const [checkoutInfo, setCheckoutInfo] = useState<CheckoutContent | null>(
-    null,
-  );
+  const { checkoutId } = useParams();
+  // TODO: checkoutId가 존재하지 않을 때 분기처리 필요
+  const {
+    checkoutContent,
+    requestUpdateCheckoutRemoteArea,
+    remoteAreaAsyncState,
+  } = useCheckoutContent(Number(checkoutId));
+
   const [isCheckoutCouponModalOpen, setIsCheckoutCouponModalOpen] =
     useState(false);
+  const [remoteAreaChecked, setRemoteAreaChecked] = useState(false);
 
-  if (!checkoutInfo) return "로딩중...";
-  const { checkoutItems } = checkoutInfo;
+  if (!checkoutContent) return "로딩중...";
+  const { checkoutItems } = checkoutContent;
+
   return (
     <div>
       <div>주문 확인</div>
+      <div>{`총 ${checkoutItems.length}종류의 상품 n개를 주문합니다. 최종 결제금액을 확인해 주세요.`}</div>
       <CheckoutItemList checkoutItems={checkoutItems} />
       <CheckoutCouponApplyButton />
+      <label>
+        <input
+          type="checkbox"
+          checked={remoteAreaChecked}
+          disabled={remoteAreaAsyncState.status === "loading"}
+          onChange={(e) =>
+            requestUpdateCheckoutRemoteArea(e.target.checked, {
+              onSuccess: ({ remoteArea: updatedRemoteArea }) =>
+                setRemoteAreaChecked(updatedRemoteArea),
+              onFail: (error) => alert(error.message),
+              showLoading: true,
+            })
+          }
+        />
+        제주도 및 도서 산간 지역
+      </label>
       <CheckoutPaymentSummary />
 
       <CheckoutCouponModal
