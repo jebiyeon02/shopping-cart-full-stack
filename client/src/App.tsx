5ccd399d844ab8daa055b750871a477f52068478
jebiyeon02 <jhje5595@naver.com>
2026-06-21 21:37:10 +0900

    refactor: 기존 주문확인 페이지를 결제완료 페이지로 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CartPage from "./pages/CartPage/CartPage";
-import OrderConfirmPage from "./pages/OrderConfirmPage/OrderConfirmPage";
+
 import { CartProvider } from "./pages/CartPage/CartContext";
 import { CartSelectionProvider } from "./pages/CartPage/CartSelectionContext";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import OrderCompletePage from "./pages/OrderCompletePage/OrderCompletePage";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         }
       />
       <Route path="checkout/:checkoutId" element={<CheckoutPage />} />
-      <Route path="cart/order-confirm" element={<OrderConfirmPage />} />
+      <Route path="order-complete" element={<OrderCompletePage />} />
     </Routes>
   );
 }
